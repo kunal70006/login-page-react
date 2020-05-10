ec5fe89ad41131d9f4493230907d91aac76d881e
Kunal <singhkunaldeep@gmail.com>
2020-05-10 20:03:39 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import React from "react";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <form action="" method="POST">
+        <h1>Register</h1>
+        <br />
+        <input
+          type="text"
+          className="username"
+          placeholder="Username"
+          required
+        />
+        <br />
+        <input
+          type="password"
+          className="password"
+          placeholder="Password"
+          required
+        />
+        <br />
+        <input
+          type="text"
+          className="email"
+          placeholder="Email Address"
+          required
+        />
+        <br />
+        <button className="btn">Register</button>
+        <br />
+        <a href="#">Already have an account?</a>
+      </form>
+    </div>
+  );
 };
 
 export default App;
